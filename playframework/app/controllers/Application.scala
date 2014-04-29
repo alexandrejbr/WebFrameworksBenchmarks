96576e8bf4675f336f5cc98c398898cf51c8b333
Alexandre Rodrigues <alexandrejbr@alexandres-mbp.home>
2014-04-30 00:17:32 +0100

    added missing controller logic

--- playframework/app/controllers/Application.scala
+++ playframework/app/controllers/Application.scala
@@ -5,6 +5,8 @@ import play.api.mvc._
 import models.Customer
 import play.api.cache.Cached
 import play.api.Play.current
+import models.Book
+import play.api.libs.json._
 
 object Application extends Controller {
   private val maxCount = 200
@@ -17,5 +19,22 @@ object Application extends Controller {
       }))
     }
   //}
+  
+  val txt = Json.stringify(Json.toJson(Book.all))
+  
+  def json = Action {
+    
+    Ok(Json.toJson(Book.all))
+  }
+  
+  def sprayjson = Action {
+    import spray.json._
+    import models.MyJsonProtocol._
+    Ok(Book.all.toJson.compactPrint)
+  }
+  
+  def text = Action {
+    Ok(txt)
+  }
 
 }
