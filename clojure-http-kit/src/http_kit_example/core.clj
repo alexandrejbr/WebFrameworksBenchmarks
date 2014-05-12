5f557e7f583001ea64dc6b0e802e8cbf811cf2be
Mehul Ira <mehul-s-ira@telecom.pt>
2014-05-12 14:25:03 +0100

    http-kit example refactored

--- clojure-http-kit/src/http_kit_example/core.clj
+++ clojure-http-kit/src/http_kit_example/core.clj
@@ -19,23 +19,31 @@
 	 :publisher "Vintage"
 	 :isbn13 9780099284864
 	 :publishedDate "April 27, 2014"}))
-
+	 
 (def msg_text (json/write-str msg))
 
 (defn json [] 
  	{:status 200
  	 :headers {"Content-Type" "application/json"}
- 	 :body (json/write-str msg)
-	})
+ 	 :body (json/write-str msg)})
 
 (defn text [] 
-	{:status  200
+	{:status 200
 	 :headers {"Content-Type" "text/plain"}
 	 :body msg_text})
 
+(defn not-found []
+	{:status 404
+	 :headers {"Content-Type" "text/plain"}
+	 :body "Not found!"})
+
+(def routing-map
+	{"/json" json
+	 "/text" text})
+
 (defn app [req] 
 	"Http request handler"
-	(if (= (get req :uri) "/json") (json) (text)))
+	((get routing-map (get req :uri) not-found)))
 
 (defn -main
 	"Application entry point"
