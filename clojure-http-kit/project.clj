23fe300043d928b36283ef2c21c0299cb69b6c54
Alexandre Rodrigues <alexandrejbr@alexandres-mbp.home>
2014-08-19 00:59:57 +0100

    update clojure configurations

--- clojure-http-kit/project.clj
+++ clojure-http-kit/project.clj
@@ -1,8 +1,10 @@
 (defproject http-kit-example "0.2.0"
   :description "http-kit and various json encoding libraries benchmark"
-  :main http-kit-example.core            
+  :main http-kit-example.core
+  :uberjar-name "http-kit-example-uberjar.jar"
+  :aot [http-kit-example.core]            
   :dependencies [[org.clojure/clojure "1.6.0"] 
-  				 [http-kit "2.1.16"]
+  				 [http-kit "2.1.18"]
   				 [clj-json "0.5.3"]
   				 [org.clojure/data.json "0.2.4"]
   				 [cheshire "5.3.1"]])
