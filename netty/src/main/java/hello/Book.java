dfb1ace5240815f967a7a95249d0cc24a67a9b9c
Alexandre Rodrigues <alexandrejbr@alexandres-mbp.home>
2014-08-18 23:06:27 +0100

    betty version first commit

---
+++ netty/src/main/java/hello/Book.java
@@ -0,0 +1,38 @@
+package hello;
+
+public class Book {
+
+    private final String title;
+    private final String[] authors;
+    private final String publisher;
+    private final long isbn13;
+    private final String publishedDate;
+
+    public Book(String title, String[] authors, String publisher, long isbn13, String publishedDate) {
+        this.title = title;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.isbn13 = isbn13;
+        this.publishedDate = publishedDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public long getIsbn13() {
+        return isbn13;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+}
