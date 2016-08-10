f856f423b11385619dfefa2b8f75eefb7f49f84f
Alexandre Rodrigues <alexandrejbr@live.com>
2016-08-10 20:20:14 +0200

    Update clojure-http-kit language and library versions

--- clojure-http-kit/project.clj
+++ clojure-http-kit/project.clj
@@ -3,8 +3,8 @@
   :main http-kit-example.core
   :uberjar-name "http-kit-example-uberjar.jar"
   :aot [http-kit-example.core]            
-  :dependencies [[org.clojure/clojure "1.6.0"] 
-  				 [http-kit "2.1.18"]
+  :dependencies [[org.clojure/clojure "1.8.0"] 
+  				 [http-kit "2.2.0"]
   				 [clj-json "0.5.3"]
   				 [org.clojure/data.json "0.2.4"]
-  				 [cheshire "5.3.1"]])
+  				 [cheshire "5.6.3"]])
