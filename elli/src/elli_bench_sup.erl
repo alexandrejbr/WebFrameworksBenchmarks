6c3e57f04e7c3560cd8782d79c35958de0a44d3e
Alexandre Rodrigues <alexandre.rodrigues@klarna.com>
2016-08-11 22:48:54 +0200

    Add erlang elli benchmark without dynamic dates

---
+++ elli/src/elli_bench_sup.erl
@@ -0,0 +1,41 @@
+-module(elli_bench_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%% CAUTION : Assign big number to db pool will fail travis ci.
+%%           Original value was 5000, too big! Even 512 crashes! keep 256
+%%           till travis-ci environment accepts bigger size of db pool. 
+
+init([]) ->
+    crypto:start(),
+    ElliOpts = [{callback, elli_bench_cb}, {port, 8080}],
+    ElliSpec = {
+        fancy_http,
+        {elli, start_link, [ElliOpts]},
+        permanent,
+        256,
+        worker,
+        [elli]},
+
+    {ok, { {one_for_one, 5, 10}, [ElliSpec]} }.
+
